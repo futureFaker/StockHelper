1174aee765b5c851fbd5a1e11379dc140268bae4
kid <lckid254253229@gmail.com>
2019-10-15 16:14:52 +0800

    1.增加全屏K线图 标题 2.增加跳转全屏K线图切换到选择的tab

--- app/src/main/java/com/lc/stockhelper/ui/StockDetailsActivity.kt
+++ app/src/main/java/com/lc/stockhelper/ui/StockDetailsActivity.kt
@@ -8,22 +8,20 @@ import com.gyf.barlibrary.ImmersionBar
 import com.lc.stockhelper.R
 import com.lc.stockhelper.data.Stock
 import com.lc.stockhelper.data.db.StockDao
-import com.lc.stockhelper.data.event.SelectedStockChangeEvent
 import com.lc.stockhelper.data.server.StockServerApi
 import com.lc.stockhelper.ui.adapter.SimpleFragmentPagerAdapter
 import com.lc.stockhelper.ui.base.ToolbarActivity
 import com.lc.stockhelper.ui.fragment.ChartKLineFragment
 import com.lc.stockhelper.utils.DialogUtils
-import com.lc.stockhelper.utils.EventBusUtils
 import kotlinx.android.synthetic.main.activity_stock_details.*
 import kotlinx.coroutines.launch
 
 /**
  * Created by KID on 2019-09-30.
  */
-class StockDetailsActivity : ToolbarActivity(){
+class StockDetailsActivity : ToolbarActivity() {
 
-    companion object{
+    companion object {
         const val KEY_STOCK = "key_stock"
     }
 
@@ -31,7 +29,7 @@ class StockDetailsActivity : ToolbarActivity(){
         return R.layout.activity_stock_details
     }
 
-    lateinit var stock:Stock
+    lateinit var stock: Stock
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,20 +41,20 @@ class StockDetailsActivity : ToolbarActivity(){
             .init()
 
         setTitleText(stock.name)
-        setTitle2Text("("+stock.code+")")
+        setTitle2Text("(" + stock.code + ")")
 
         setToolbarBackground(stock.getTypeColor())
         enableNavigation()
 
         setRightIvClickListener {
-            if(stock.selected){
+            if (stock.selected) {
                 stock.selected = false
                 showStockSelected()
                 launch {
                     StockDao.removeSelectedStock(stock)
 
                 }
-            }else{
+            } else {
                 stock.selected = true
                 showStockSelected()
                 launch {
@@ -66,20 +64,20 @@ class StockDetailsActivity : ToolbarActivity(){
         }
 
         setRightIv2ClickListener {
-            if(!stock.bought){
-                DialogUtils.showBuyStockDialog(this){
+            if (!stock.bought) {
+                DialogUtils.showBuyStockDialog(this) {
                     stock.bought = true
                     showStockBought()
                     launch {
-                        StockDao.addBoughtStock(stock,it)
+                        StockDao.addBoughtStock(stock, it)
                     }
                 }
-            }else{
-                DialogUtils.showSellStockDialog(this){
+            } else {
+                DialogUtils.showSellStockDialog(this) {
                     stock.bought = false
                     showStockBought()
                     launch {
-                        StockDao.removeBoughtStock(stock,it)
+                        StockDao.removeBoughtStock(stock, it)
                     }
                 }
             }
@@ -90,49 +88,65 @@ class StockDetailsActivity : ToolbarActivity(){
         getStockDetails()
     }
 
-    private fun showStockSelected(){
-        if(stock.selected){
+    private fun showStockSelected() {
+        if (stock.selected) {
             setRightIvRes(R.drawable.ic_cancel_self_selected_1)
-        }else{
+        } else {
             setRightIvRes(R.drawable.ic_add_self_selected_1)
         }
     }
 
-    private fun showStockBought(){
-        if(stock.bought){
+    private fun showStockBought() {
+        if (stock.bought) {
             setRightIv2Res(R.drawable.ic_sale_1)
-        }else{
+        } else {
             setRightIv2Res(R.drawable.ic_buy_1)
         }
     }
 
-    private fun getStockDetails(){
+    private fun getStockDetails() {
         launch {
             val dayData = StockServerApi.getStockDetails(stock)
             val weekData = StockServerApi.getStockWeekDetails(stock)
-            if(!dayData.isEmpty()){
+            if (!dayData.isEmpty()) {
 
-                val clickLisener = View.OnClickListener {
-                    val intent = Intent(this@StockDetailsActivity, StockDetailsLandActivity::class.java)
-                    intent.putExtra(StockDetailsLandActivity.KEY_DAY_DATA, dayData)
-                    intent.putExtra(StockDetailsLandActivity.KEY_WEEK_DATA, weekData)
-                    startActivity(intent)
-                }
 
                 val fragments = arrayOf<Fragment>(
-                    ChartKLineFragment.newInstance(ChartKLineFragment.TYPE_DAY,dayData,false).apply {
-                        onClickListener = clickLisener
+                    ChartKLineFragment.newInstance(
+                        ChartKLineFragment.TYPE_DAY,
+                        dayData,
+                        false
+                    ).apply {
+                        onClickListener = View.OnClickListener {
+                            val intent =
+                                Intent(this@StockDetailsActivity, StockDetailsLandActivity::class.java)
+                            intent.putExtra(StockDetailsLandActivity.KEY_STOCK, stock)
+                            intent.putExtra(StockDetailsLandActivity.KEY_DAY_DATA, dayData)
+                            intent.putExtra(StockDetailsLandActivity.KEY_WEEK_DATA, weekData)
+                            intent.putExtra(StockDetailsLandActivity.KEY_INDEX,0)
+                            startActivity(intent)
+                        }
                     },
-                    ChartKLineFragment.newInstance(ChartKLineFragment.TYPE_WEEK,weekData, false).apply {
-                        onClickListener = clickLisener
+                    ChartKLineFragment.newInstance(
+                        ChartKLineFragment.TYPE_WEEK,
+                        weekData,
+                        false
+                    ).apply {
+                        onClickListener = View.OnClickListener {
+                            val intent =
+                                Intent(this@StockDetailsActivity, StockDetailsLandActivity::class.java)
+                            intent.putExtra(StockDetailsLandActivity.KEY_STOCK, stock)
+                            intent.putExtra(StockDetailsLandActivity.KEY_DAY_DATA, dayData)
+                            intent.putExtra(StockDetailsLandActivity.KEY_WEEK_DATA, weekData)
+                            intent.putExtra(StockDetailsLandActivity.KEY_INDEX,1)
+                            startActivity(intent)
+                        }
                     }
                     //ChartKLineFragment.newInstance(30, false)
                 )
 
 
-
-
-                val titles = arrayOf("日K","周K")
+                val titles = arrayOf("日K", "周K")
                 viewPager.setOffscreenPageLimit(fragments.size)
                 viewPager.setAdapter(
                     SimpleFragmentPagerAdapter(
--- app/src/main/java/com/lc/stockhelper/ui/StockDetailsLandActivity.kt
+++ app/src/main/java/com/lc/stockhelper/ui/StockDetailsLandActivity.kt
@@ -2,34 +2,54 @@ package com.lc.stockhelper.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.gyf.barlibrary.ImmersionBar
 import com.lc.stockhelper.R
+import com.lc.stockhelper.data.Stock
 import com.lc.stockhelper.ui.adapter.SimpleFragmentPagerAdapter
-import com.lc.stockhelper.ui.base.CoroutineActivity
+import com.lc.stockhelper.ui.base.ToolbarActivity
 import com.lc.stockhelper.ui.fragment.ChartKLineFragment
 import kotlinx.android.synthetic.main.activity_stock_details.*
 
 /**
  * Created by KID on 2019-10-11.
  */
-class StockDetailsLandActivity : CoroutineActivity() {
+class StockDetailsLandActivity : ToolbarActivity() {
 
     companion object {
+        const val KEY_STOCK = "key_stock"
         const val KEY_DAY_DATA = "key_day_data"
         const val KEY_WEEK_DATA = "key_week_data"
+        const val KEY_INDEX = "key_index"
+    }
+
+    override fun layoutId(): Int {
+        return R.layout.activity_stock_detail_land
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_stock_detail_land)
-
         val dayData = intent.extras.getString(KEY_DAY_DATA)
         val weekData = intent.extras.getString(KEY_WEEK_DATA)
+
+        val stock = intent.extras.getParcelable(KEY_STOCK) as Stock
+
+        val index = intent.extras.getInt(KEY_INDEX)
+        ImmersionBar.with(this)
+            .statusBarColor(stock.getTypeColor())
+            .fitsSystemWindows(true)
+            .init()
+
+        setTitleText(stock.name)
+        setTitle2Text("(" + stock.code + ")")
+
+        setToolbarBackground(stock.getTypeColor())
+        enableNavigation()
 
         val fragments = arrayOf<Fragment>(
             ChartKLineFragment.newInstance(ChartKLineFragment.TYPE_DAY, dayData, true),
             ChartKLineFragment.newInstance(ChartKLineFragment.TYPE_WEEK, weekData, true)
         )
-        val titles = arrayOf("日K","周K")
+        val titles = arrayOf("日K", "周K")
         viewPager.setOffscreenPageLimit(fragments.size)
         viewPager.setAdapter(
             SimpleFragmentPagerAdapter(
@@ -39,6 +59,7 @@ class StockDetailsLandActivity : CoroutineActivity() {
             )
         )
         tabLayout.setupWithViewPager(viewPager)
+        viewPager.currentItem = index
     }
 
 }
